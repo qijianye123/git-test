9e2a348bba387443b5bbde17dcaf1aa3cbf5c8a7
qijianye <qijianye@qq.com>
2022-02-19 14:29:06 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/test/Test.java
+++ src/main/java/com/atguigu/test/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("hello world");
         System.out.println("hello world222");
         System.out.println("hello world3333");
+        System.out.println("hello world444444");
     }
 }
