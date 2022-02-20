4e48562eaa12940cd1ec248edb38e44c89a10ffb
qijianye123 <99526153+qijianye123@users.noreply.github.com>
2022-02-20 10:06:09 +0800

    删除了一些代码

--- src/main/java/com/atguigu/test/Test.java
+++ src/main/java/com/atguigu/test/Test.java
@@ -5,9 +5,6 @@ public class Test {
         System.out.println("hello world");
         System.out.println("hello world222");
         System.out.println("hello world3333");
-        System.out.println("hello world444444");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
+       
     }
 }
